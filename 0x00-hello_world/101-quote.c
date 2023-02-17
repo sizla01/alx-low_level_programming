e8c33bbc9ccb6234c15f7bfc3bffa1d6d9cb1848
sizla01 <faithwambui735@gmail.com>
2023-02-17 00:48:43 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main -a c program that to the standard error
+ * Return: 0
+ */
+int main(void)
+{
+puts("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+return(1);
+}
