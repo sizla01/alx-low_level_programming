2e8f2a917872008cf7ae2f37b04f68b49394d4d4
sizla01 <faithwambui735@gmail.com>
2023-02-16 23:44:27 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -a c program that prints using printf
+ * Return: 0
+ */
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
