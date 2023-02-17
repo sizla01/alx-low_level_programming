57648d804c84980bb5fc16f6f55a43ca3c1115aa
sizla01 <faithwambui735@gmail.com>
2023-02-17 13:33:21 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main fuction
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char j;
+
+	for (j = 'z'; j >= 'a'; j--)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
