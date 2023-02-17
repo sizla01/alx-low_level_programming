4818cd9c03cb12a578000c70f3f7b22f333f6de1
sizla01 <faithwambui735@gmail.com>
2023-02-17 15:50:23 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,18 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 0; i < 9; i++)
+	for (i =0; i < 10; i++)
 	{
-		for (j = i + 1; j < 10; j++)
+		for (j = 1; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 && j != 9)
+			if (i < j && i != j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i + '0');
+				putchar(j + '0');
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -mainfuction
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
