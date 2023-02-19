972a867a19603f69c3a7789cbf1cb096fadcb2e3
sizla01 <faithwambui735@gmail.com>
2023-02-19 08:58:47 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
- * main -a c program that to the standard error
- * Return: 0
+ * main - a c program that to the standard error
+ * Return: 1
  */
 int main(void)
 {
-puts("and that piece of art is useful" - Dora Korpar, 2015-10-19");
-return(1);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
 }
