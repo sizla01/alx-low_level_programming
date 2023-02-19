6ffd524004dd1e50441674b4c84122929428a912
sizla01 <faithwambui735@gmail.com>
2023-02-19 09:42:06 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,16 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)", sizeof(char));
-	printf("Size of an int: %lu byte(s)", sizeof(int));
-	printf("Size of a long int: %lu byte(s)", sizeof(long int));
-	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
-	printf("Size of a float: %lu byte(s)", sizeof(float));
+	char forchar;
+	int forint;
+	long int forlongint;
+	long long int forlonglongint;
+	float forfloat;
+
+	printf("Size of a char: %lu byte(s)", sizeof(forchar));
+	printf("Size of an int: %lu byte(s)", sizeof(forint));
+	printf("Size of a long int: %lu byte(s)", sizeof(forlongint));
+	printf("Size of a long long int: %lu byte(s)", sizeof(forlonglongint));
+	printf("Size of a float: %lu byte(s)", sizeof(forfloat));
 	return (0);
 }
