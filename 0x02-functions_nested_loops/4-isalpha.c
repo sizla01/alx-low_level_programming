7ae6a3a230ada6a9a26fcaa7bfd9128d63d1926c
sizla01 <faithwambui735@gmail.com>
2023-02-21 06:45:49 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: parameter to be printed
+ *
+ * Return: 1 for alphabetic or 0 anything else
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90 ) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
