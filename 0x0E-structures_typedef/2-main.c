42e9a9f9ce15cb02e1e85158fc1711af3077f475
sizla01 <faithwambui735@gmail.com>
2023-03-20 04:19:34 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-main.c
@@ -0,0 +1,19 @@
+julien@ubuntu:~/0x0d. structures, typedef$ cat 2-main.c
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Poppy";
+	my_dog.age = 3.5;
+	my_dog.owner = "Bob";
+	print_dog(&my_dog);
+	return (0);
+}
