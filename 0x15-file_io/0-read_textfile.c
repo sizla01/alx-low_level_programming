f0a2212460653125cdb2114acba8f32279d81a75
sizla01 <faithwambui735@gmail.com>
2023-04-09 01:25:28 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,26 +1,49 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
- * @filename: file that reads and write
- * * @letters: number of letters to read and write.
+* read_textfile - check the code for Holberton School students.
+* @filename: file to read and write
+* @letters: number of letters to read and write.
 * Return: letters printed
 */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	char *buf;
-	ssize_t fd;
-	ssize_t x;
-	ssize_t y;
 
-	fd = open(filename, O_WRONLY);
-	if (fd == -1)
+ssize_t read_textfile(const char *filename, size_t letters)
+
+{
+	ssize_t nletters;
+
+	int file;
+
+	char *text;
+
+	if (!filename)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	y = read(fd, buf, letters);
-	x = write(STDOUT_FILENO, buf, y);
-	free(buf);
-	close(fd);
-	return (x);
+	text = malloc(sizeof(char) * letters + 1);
+
+	if (text == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+
+	{
+		free(text);
+		return (0);
+	}
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (nletters);
 }
